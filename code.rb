96800f10b7e6eb81d6c1a754e456178b571461c5
Sarah <nellamsem@gmail.com>
2017-02-25 17:00:46 -0800

    Add files via upload
    Cleaned up some instance methods

--- code.rb
+++ code.rb
@@ -1,4 +1,19 @@
 class Ingredient
+  VALID_INGREDIENTS = [
+    "brussels_sprouts",
+    "spinach",
+    "eggs",
+    "milk",
+    "tofu",
+    "seitan",
+    "bell_peppers",
+    "quinoa",
+    "kale",
+    "chocolate",
+    "beer",
+    "wine",
+    "whiskey"
+  ]
   attr_reader :summary
   def initialize(quantity, unit, name)
     @quantity, @unit, @name = quantity, unit, name
@@ -6,9 +21,16 @@ class Ingredient
   def summary
     @summary = "#{@quantity} #{@unit} #{@name}"
   end
+
+  def self.parse(info)
+    summary = info.split(" ")
+    new(summary[0].to_f, summary[1], summary[2])
+  end
+
+  def safe_ingredient?
+    VALID_INGREDIENTS.include?(@name)
+  end
 end
-ingredient = Ingredient.new(47.0, "lb(s)", "Brussels Sprouts")
-puts ingredient.summary
 
 class Recipe
   attr_reader :summary
@@ -17,51 +39,30 @@ class Recipe
   end
 
   def summary
-    puts "\nRecipe: #{@name}\n"
-    puts "\nIngredients:"
-    @ingredients.each do |item|
-      puts "- #{item.summary}"
-    end
+    @summary =
+    puts "\nRecipe: #{@name}\n\nIngredients:"
+      @ingredients.each do |item|
+        puts "- #{item.summary}\n"
+      end
     puts "\nInstructions:"
-    @instructions.each_with_index do |step, num|
-      puts "#{num + 1}. #{step}"
-    end
-    return
+      @instructions.each_with_index do |step, num|
+        "#{num + 1}. #{step}"
+      end
+  end
+  def safe_recipe?
+    @ingredients.all? { |item| item.safe_ingredient? }
   end
 end
 
-ingredients_a = [
-    Ingredient.new(1.0, "cup", "half & half"),
-    Ingredient.new(3.0, "", "large eggs"),
-    Ingredient.new(2.0, "tbsp", "honey (warmed in microwave for 20 seconds)"),
-    Ingredient.new(0.25, "tsp", "salt"),
-    Ingredient.new(8.0, "1/2-inch slices", "bread"),
-    Ingredient.new(4.0,  "tbsp", "butter")
-  ]
-
-instructions_a = "In medium size mixing bowl, whisk together the half-and-half, eggs, honey, and salt. You may do this the night before.
-When ready to cook, pour custard mixture into a pie pan and set aside.
-Preheat oven to 375 degrees F.
-Dip bread into mixture, allow to soak for 30 seconds on each side, and then remove to a cooling rack that is sitting in a sheet pan, and allow to sit for 1 to 2 minutes.
-Over medium-low heat, melt 1 tablespoon of butter in a 10-inch nonstick saute pan.
-Place 2 slices of bread at a time into the pan and cook until golden brown, approximately 2 to 3 minutes per side.
-Remove from pan and place on rack in oven for 5 minutes.
-Repeat with all 8 slices.
-Serve immediately with maple syrup, whipped cream or fruit.".split("\n")
-
-a = Recipe.new("French Toast", instructions_a, ingredients_a)
-# *******http://www.foodnetwork.com/recipes/alton-brown/french-toast-recipe*******
-puts a.summary
-
 ingredients = [
-  Ingredient.new(0.67, "cups", "undiluted Carnation Evaporated Milk"),
-  Ingredient.new(1.67, "cups", "sugar"),
-  Ingredient.new(0.5, "tsp", "salt"),
-  Ingredient.new(2.0, "tbsp", "butter"),
-  Ingredient.new(2.0, "cups", "miniature marshmallows or diced marshmallows"),
-  Ingredient.new(0.5, "cups", "chopped nuts"),
-  Ingredient.new(1.5, "cups", "semi-sweet chocolate chips"),
-  Ingredient.new(1.0, "tsp", "vanilla")
+  # Ingredient.new(0.67, "cups", "undiluted Carnation Evaporated Milk"),
+  # Ingredient.new(1.67, "cups", "sugar"),
+  # Ingredient.new(0.5, "tsp", "salt"),
+  # Ingredient.new(2.0, "tbsp", "butter"),
+  # Ingredient.new(2.0, "cups", "miniature marshmallows or diced marshmallows"),
+  # Ingredient.new(0.5, "cups", "chopped nuts"),
+  # Ingredient.new(1.5, "cups", "semi-sweet chocolate chips"),
+  Ingredient.new(1.0, "tsp", "beer")
 ]
 instructions = [ "Put Carnation, sugar, salt, and butter in saucepan and put on stove.",
   "Turn burner to medium. Heat to boiling; stir often.",
@@ -72,4 +73,8 @@ instructions = [ "Put Carnation, sugar, salt, and butter in saucepan and put on 
   "When the fudge is cool, cut into squares."
 ]
 b = Recipe.new("Five Minute Fudge", instructions, ingredients)
+puts b.safe_recipe?
+# c = Ingredient.parse("5 tspn(s) milk")
 puts b.summary
+c = Ingredient.parse("2 cups water")
+puts c.summary
